5be40f98d0c9ee824b7840c101b78eed735b2ef1
9jiya <74593564+9jiya@users.noreply.github.com>
2021-06-03 19:18:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+img = "";
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+function draw(){
+    image(img,0,0,640,420);
+    fill("red");
+    stroke("red");
+    noFill();
+    rect(200,60,100,250);
+    text("Dog",200,70);
+}
